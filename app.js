9ea1abcdd71d627f760cdd20032a01cbac69f14d
AlexTWilson95 <Alexander.wilson5@ontariotechu.net>
2025-11-26 23:27:39 -0500

    app.js update and json updates

--- app.js
+++ app.js
@@ -1,27 +1,47 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const methodOverride = require("method-override");
+const session = require("express-session");
 
 const app = express();
 
-// middleware
+// Import Routes
+const authRoutes = require("./routes/auth");
+const indexRoutes = require("./routes/index");
+const workoutRoutes = require("./routes/workouts");
+
+
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
 app.use(express.static("public"));
-app.set("view engine", "ejs");
 
-// connect to MongoDB
+app.use(
+  session({
+    secret: "super-secret-key",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
-// ROUTES
-app.get("/", (req, res) => {
-  res.render("index",);
-});
+// Use Routes
+app.use("/", authRoutes);
+app.use("/", indexRoutes);
+app.use("/workouts", workoutRoutes);
 
+// Start Server
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log("Server running on port " + port));
+
